83a89f6aeb1ead27244253a4c7c4f3bd01667630
Kerim Amansaryyev <keamansaryyev@gmail.com>
2022-08-18 04:31:46 +0500

    Updated test

--- test/adaptix_test.dart
+++ test/adaptix_test.dart
@@ -1,7 +1,9 @@
 import 'argument_classes_test.dart' as arg_test;
 import 'device_detection_test.dart' as pixel_scale_test;
+import 'initializer_widget_test.dart' as initializer_testing;
 
 void main() {
   arg_test.main();
   pixel_scale_test.main();
+  initializer_testing.main();
 }
