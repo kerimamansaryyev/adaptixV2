2bd34b4c037bf9316957dce7953c2d0dc3354bac
Kerim Amansaryyev <keamansaryyev@gmail.com>
2022-08-18 19:20:57 +0500

    Canonical extension names

--- lib/src/extensions/detect_breakpoint_extension.dart
+++ lib/src/extensions/detect_breakpoint_extension.dart
@@ -1,27 +1,34 @@
+import 'package:adaptix/src/models/breakpoint.dart';
 import 'package:adaptix/src/models/pixel_scale_breakpoint.dart';
 import 'package:meta/meta.dart';
 
-extension DetectBreakpointExtension on List<ResponsivePixelScaleBreakPoint> {
-  ResponsivePixelScaleBreakPoint? _detectBreakpoint(double deviceWidth) {
+extension DetectBreakpointExtension on Iterable<SizeBreakPoint> {
+  SizeBreakPoint? detectBreakpoint(double deviceWidth) {
     if (isEmpty) {
       return null;
-    } else if (deviceWidth < first.deviceWidth) {
+    } else if (deviceWidth < first.compareValue) {
       return first;
     }
 
     for (int i = 1; i < length; i++) {
-      if (deviceWidth < elementAt(i).deviceWidth) {
+      if (deviceWidth < elementAt(i).compareValue) {
         return elementAt(i - 1);
       }
     }
 
     return last;
   }
+}
+
+extension DetectBreakpointPixelScaleExtension
+    on Iterable<ResponsivePixelScaleBreakPoint> {
+  ResponsivePixelScaleBreakPoint? _detectBreakPoint(double deviceWidth) =>
+      detectBreakpoint(deviceWidth) as ResponsivePixelScaleBreakPoint?;
 
   double detectPixelScale(double deviceWidth, [double defaultScale = 1]) =>
-      _detectBreakpoint(deviceWidth)?.pixelScale ?? defaultScale;
+      _detectBreakPoint(deviceWidth)?.pixelScale ?? defaultScale;
 
   @visibleForTesting
   String? detectDeviceTypeByDebugLabel(double deviceWidth) =>
-      _detectBreakpoint(deviceWidth)?.debugLabel;
+      _detectBreakPoint(deviceWidth)?.debugLabel;
 }
