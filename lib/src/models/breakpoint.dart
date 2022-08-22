63454c3448298ba0009686c36f9c741d18323d30
Kerim Amansaryyev <keamansaryyev@gmail.com>
2022-08-23 01:33:55 +0500

    Set value of desktop breakpoint to 1020

--- lib/src/models/breakpoint.dart
+++ lib/src/models/breakpoint.dart
@@ -7,7 +7,7 @@ enum CanonicalResponsiveBreakpoint implements ResponsiveBreakpoint {
   small(320, smallKey),
   medium(414, mediumKey),
   tablet(720, tabletKey),
-  desktop(1024, desktopKey);
+  desktop(1020, desktopKey);
 
   static const xSmallKey = 'xSmall';
   static const smallKey = 'small';
