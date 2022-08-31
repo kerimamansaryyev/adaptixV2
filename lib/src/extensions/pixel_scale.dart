d9d96446fdff4eb6d024f7314c147934252a1b61
Kerim Amansaryyev <keamansaryyev@gmail.com>
2022-09-01 00:27:16 +0500

    Removed useless parameter defaultValue from .adaptedPx extension method

--- lib/src/extensions/pixel_scale.dart
+++ lib/src/extensions/pixel_scale.dart
@@ -2,7 +2,7 @@ import 'package:adaptix/src/widgets/adaptix.dart';
 import 'package:flutter/widgets.dart';
 
 extension PixelScaleExtension on num {
-  double adaptedPx(BuildContext context, {double defaultValue = 1}) {
+  double adaptedPx(BuildContext context) {
     final adaptix = Adaptix.of(context);
     final genericSwitch = adaptix.configs.pixelScaleSwitch;
     return genericSwitch.getValueAccordingtoBreakpoint(adaptix.breakpoint) *
