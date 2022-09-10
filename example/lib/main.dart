d5cd3cd0aa688fa540fdd83a071266303417aaeb
Kerim Amansaryyev <keamansaryyev@gmail.com>
2022-09-10 17:15:51 +0500

    Updated the docs

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,74 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:adaptix/adaptix.dart';
+
+enum MyResposivenessPattern implements ResponsiveBreakpoint {
+  iphone7(templateDeviceWidth: 375, pixelScale: 1),
+  iphone11(templateDeviceWidth: 414, pixelScale: 1.1),
+  ipadMini(templateDeviceWidth: 768, pixelScale: 1.12),
+  someDesktopDevice(templateDeviceWidth: 1024, pixelScale: 1.2);
+
+  static final myPixelScaleRules = MyResposivenessPattern.values
+      .map<GenericResponsiveRule<double>>((devicePattern) =>
+          GenericResponsiveRule(devicePattern.key, devicePattern.pixelScale))
+      .toList();
+
+  @override
+  final double templateDeviceWidth;
+
+  final double pixelScale;
+
+  const MyResposivenessPattern(
+      {required this.templateDeviceWidth, required this.pixelScale});
+
+  @override
+  String get debugLabel => key;
+
+  @override
+  bool hasSameValueAs(ResponsiveBreakpoint other) =>
+      other.templateDeviceWidth == templateDeviceWidth;
+
+  @override
+  bool isSameAs(ArgsComparisonMixin other) {
+    return other is ResponsiveBreakpoint &&
+        other.templateDeviceWidth == templateDeviceWidth &&
+        other.key == key;
+  }
+
+  /// key as a name of an element of this enum
+  @override
+  String get key => name;
+
+  static GenericResponsiveSwitchArgs<T> myResponsiveSwitchArguments<T>(
+      {required T defaultValue,
+      T? iphone7Value,
+      T? iphone11Value,
+      T? ipadMiniValue,
+      T? someDesktopDeviceValue}) {
+    final ruleValues = [
+      iphone7Value,
+      iphone11Value,
+      ipadMiniValue,
+      someDesktopDeviceValue
+    ];
+    return GenericResponsiveSwitchArgs<T>(defaultValue: defaultValue, rules: [
+      for (int i = 0;
+          i < min(MyResposivenessPattern.values.length, ruleValues.length);
+          i++)
+        GenericResponsiveRule(
+            MyResposivenessPattern.values[i].key, ruleValues[i] ?? defaultValue)
+    ]);
+  }
+
+  static AdaptixConfigs configs() => AdaptixConfigs(
+      breakpoints: MyResposivenessPattern.values,
+      pixelScaleRules: MyResposivenessPattern.myPixelScaleRules);
+
+  /// deprecated, implement as null
+  @override
+  double? get value => null;
+}
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return AdaptixInitializer(
-        configs: const AdaptixConfigs.canonical(),
+        configs: MyResposivenessPattern.configs(),
         builder: (context) {
           return MaterialApp(
             home: Scaffold(
@@ -47,7 +116,12 @@ class _MyHomePageState extends State<MyHomePage> {
                       color: Colors.red,
                       width: 50.adaptedPx(context),
                       height: 50.adaptedPx(context),
-                      child: Text('${50.adaptedPx(context).round()} px'),
+                      child: Text(context.responsiveSwitch(
+                          MyResposivenessPattern.myResponsiveSwitchArguments(
+                              defaultValue: 'Unrecognized device pattern',
+                              iphone7Value: 'iphone7 device pattern',
+                              iphone11Value: 'iphone11 device pattern',
+                              ipadMiniValue: 'ipadMini device pattern'))),
                     )
                   ],
                 ),
